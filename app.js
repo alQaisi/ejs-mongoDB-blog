9125c06351a1fccb1cc1b21812bb131218c1fadb
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-09-11 23:15:52 +0300

    fixing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app=express();
 
 const dbURI=`mongodb+srv://ahmad:${process.env.DB_PASSWORD}@blog.ppg2upz.mongodb.net/ahmad-blogs?retryWrites=true&w=majority`;
 mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
-    .then((result)=>app.listen(3000))
+    .then((result)=>app.listen(process.env.PORT || 3000))
     .catch((err)=>console.log(err));
 
 // register view engine
